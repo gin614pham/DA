e4c718c0468729dafd68fb3cf47085b02ab6c1e7
Gin <hoangphuc442266@gmail.com>
2023-11-11 15:33:05 +0700

    update

--- main.c
+++ main.c
@@ -8,39 +8,26 @@
 #include <pwd.h>
 #include <grp.h>
 #include <time.h>
+#include <dirent.h>
 
 // Function to print file information
-void printFileInfo(const char *fileName) {
+void printFileInfo(const char *filePath) {
+    const char *fileName = strrchr(filePath, '/') ? strrchr(filePath, '/') + 1 : filePath;
+
     struct stat fileInfo;
 
     // Get file information
-    if (stat(fileName, &fileInfo) < 0) {
+    if (stat(filePath, &fileInfo) < 0) {
         perror("stat");
         return;
     }
+    printf("Information for '%s':\n", fileName);
 
     // Check if it's a directory
-    if (S_ISDIR(fileInfo.st_mode)) {
-        printf("Directory: %s\n", fileName);
-    } else if (S_ISREG(fileInfo.st_mode)) {
-        printf("File: %s\n", fileName);
-    } else {
-        printf("Other: %s\n", fileName);
-    }
+    printf("%s: %s\n", S_ISDIR(fileInfo.st_mode) ? "Directory" : "File", fileName);
 
     printf("Number of Links: %ld\n", (long)fileInfo.st_nlink);
-    printf("File Permissions: ");
-    printf((S_ISDIR(fileInfo.st_mode)) ? "d" : "-");
-    printf((fileInfo.st_mode & S_IRUSR) ? "r" : "-");
-    printf((fileInfo.st_mode & S_IWUSR) ? "w" : "-");
-    printf((fileInfo.st_mode & S_IXUSR) ? "x" : "-");
-    printf((fileInfo.st_mode & S_IRGRP) ? "r" : "-");
-    printf((fileInfo.st_mode & S_IWGRP) ? "w" : "-");
-    printf((fileInfo.st_mode & S_IXGRP) ? "x" : "-");
-    printf((fileInfo.st_mode & S_IROTH) ? "r" : "-");
-    printf((fileInfo.st_mode & S_IWOTH) ? "w" : "-");
-    printf((fileInfo.st_mode & S_IXOTH) ? "x" : "-");
-    printf("\n");
+    printf("File Permissions: %o\n", fileInfo.st_mode & 0777);
 
     struct passwd *pw = getpwuid(fileInfo.st_uid);
     struct group *gr = getgrgid(fileInfo.st_gid);
@@ -51,21 +38,138 @@ void printFileInfo(const char *fileName) {
 
     // Format and print the last access time
     char buffer[80];
-    struct tm *timeinfo = localtime(&fileInfo.st_atime);
-    strftime(buffer, 80, "Last Access Time: %Y-%m-%d %H:%M:%S", timeinfo);
+    strftime(buffer, 80, "Last Access Time: %Y-%m-%d %H:%M:%S", localtime(&fileInfo.st_atime));
     printf("%s\n", buffer);
 
     printf("\n");
 }
 
-int main(int argc, char *argv[]) {
-    if (argc < 2) {
-        printf("Usage: %s [file/directory1] [file/directory2] ...\n", argv[0]);
-        exit(1);
+void createFile(const char *fileName) {
+    // create a file
+    FILE *fp = fopen(fileName, "w");
+    if (fp != NULL) {
+        fclose(fp);
+        printf("File '%s' created successfully.\n", fileName);
+    } else {
+        perror("fopen");
     }
+}
 
-    for (int i = 1; i < argc; i++) {
-        printFileInfo(argv[i]);
+void deleteFile(const char *fileName) {
+    // delete a file
+    if (remove(fileName) == 0) {
+        printf("File '%s' deleted successfully.\n", fileName);
+    } else {
+        perror("remove");
+    }
+}
+
+void renameFile(const char *oldName, const char *newName) {
+    // rename a file
+    if (rename(oldName, newName) == 0) {
+        printf("File '%s' renamed to '%s' successfully.\n", oldName, newName);
+    } else {
+        perror("rename");
+    }
+}
+
+void moveFile(const char *source, const char *destination) {
+    // move a file
+    if (rename(source, destination) == 0) {
+        printf("File '%s' moved to '%s' successfully.\n", source, destination);
+    } else {
+        perror("rename");
+    }
+}
+
+void listFiles(const char *directoryPath) {
+    // list files in a directory
+    DIR *dir = opendir(directoryPath);
+    if (dir != NULL) {
+        struct dirent *entry;
+        printf("Files in directory '%s':\n", directoryPath);
+        while ((entry = readdir(dir)) != NULL) {
+            printf("%s\n", entry->d_name);
+        }
+        closedir(dir);
+    } else {
+        perror("opendir");
+    }
+}
+
+int main(int argc, char *argv[]) {
+    if (argc >= 2) {
+        // check if the first argument is "-h" or "-help"
+        if (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "-help") == 0) {
+            // print help message
+            printf("Usage: ./myFileManager -c file_name to create a file\n");
+            printf("Usage: ./myFileManager -d file_name to delete a file\n");
+            printf("Usage: ./myFileManager -r old_file_name new_file_name to rename a file\n");
+            printf("Usage: ./myFileManager -m source_file destination_file to move a file\n");
+            printf("Usage: ./myFileManager -l directory_path to list files in a directory\n");
+            printf("Usage: ./myFileManager -i file_name to show information about a file\n");
+            printf("Usage: ./myFileManager -h or ./myFileManager -help to show help\n");
+            return 0;
+        }
+        // switch on the first argument
+        switch (argv[1][1]) {
+            case 'c':
+                // check if there are enough arguments
+                if (argc != 3) {
+                    printf("Invalid argument\n");
+                    printf("Usage: ./myFileManager -c file_name to create a file\n");
+                }
+                createFile(argv[2]);
+                break;
+            case 'd':
+                // check if there are enough arguments
+                if (argc != 3) {
+                    printf("Invalid argument\n");
+                    printf("Usage: ./myFileManager -d file_name to delete a file\n");
+                }
+                deleteFile(argv[2]);    
+                break;
+            case 'r':
+                // check if there are enough arguments
+                if (argc != 4) {
+                    printf("Invalid argument\n");
+                    printf("Usage: ./myFileManager -r old_file_name new_file_name to rename a file\n");
+                }
+                renameFile(argv[2], argv[3]);
+                break;
+            case 'm':
+                // check if there are enough arguments
+                if (argc != 4) {
+                    printf("Invalid argument\n");
+                    printf("Usage: ./myFileManager -m source_file destination_file to move a file\n");
+                }
+                moveFile(argv[2], argv[3]);
+                break;
+            case 'l':
+                listFiles(argc > 2 ? argv[2] : ".");
+                break;
+            case 'i':
+                // check if there are enough arguments
+                if (argc != 3) {
+                    printf("Invalid argument\n");
+                    printf("Usage: ./myFileManager -i file_name to show information about a file\n");
+                }
+                printFileInfo(argv[2]);
+                break;
+            default:
+                printf("Invalid argument\n");
+                printf("Usage: ./myFileManager -c file_name to create a file\n");
+                printf("Usage: ./myFileManager -d file_name to delete a file\n");
+                printf("Usage: ./myFileManager -r old_file_name new_file_name to rename a file\n");
+                printf("Usage: ./myFileManager -m source_file destination_file to move a file\n");
+                printf("Usage: ./myFileManager -l directory_path to list files in a directory\n");
+                printf("Usage: ./myFileManager -i file_name to show information about a file\n");
+                printf("Usage: ./myFileManager -h or ./myFileManager -help to show help\n");
+                break;
+        }
+    } else {
+        // print help
+        printf("Usage: ./myFileManager -help or ./myFileManager -h to show help\n");
     }
 
     return 0;
